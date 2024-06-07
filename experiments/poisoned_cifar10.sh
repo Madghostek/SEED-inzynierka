f209daec1696ec353d4861265e96cadaa150f47b
tsroka <01169239@pw.edu.pl>
2024-06-07 12:17:21 +0200

    copy sh from athena

--- experiments/poisoned_cifar10.sh
+++ experiments/poisoned_cifar10.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #SBATCH -A plgplgdyplomancipw-gpu-a100
 #SBATCH -p plgrid-gpu-a100    
-#SBATCH --job-name=SEED-test
+#SBATCH --job-name=30op_10subset
 #SBATCH --ntasks=4                    
 #SBATCH --gpus=1
 #SBATCH --mem=3gb                     # Job memory request
 #SBATCH --time=03:00:00               # Time limit hrs:min:sec
-#SBATCH --output=SEED_TEST # %j to jobid
-for NUM_EXPERTS in 5
-do
-  python3 ../src/main_incremental.py --approach seed --gmms 1 --max-experts $NUM_EXPERTS --use-multivariate --nepochs 200 --tau 3 --batch-size 128 --num-workers 4 --datasets cifar_10_poisoned --num-tasks 5 --nc-first-task 2 --lr 0.05 --weight-decay 5e-4 --clipping 1 --alpha 0.99 --use-test-as-val --network resnet32 --extra-aug fetril --momentum 0.9 --exp-name poison_50 --seed 0
-done
-
+#SBATCH --output=finetune_20op_10subset_1# %j is jobid
+python3 ../src/main_incremental.py --approach finetuning --num-exemplars 2000 --nepochs 200 --batch-size 128 --num-workers 4 --datasets cifar_10_poisoned --num-tasks 5 --nc-first-task 2 --lr 0.05 --weight-decay 5e-4 --clipping 1 --network resnet32 --extra-aug fetril --momentum 0.9 --exp-name opacity30 --seed 1 --log tensorboard
+.
