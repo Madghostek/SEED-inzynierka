a0b237f9a746c445a836d193903df01d53474b6e
Madghostek <madghostek@gmail.com>
2025-01-13 20:39:50 +0100

    style: rename project folders

--- experiments/make_poisoned.sh
+++ experiments/make_poisoned.sh
@@ -1,7 +1,7 @@
 python3 utils/manage_dataset.py --poison-method blend-random \
 --ratio 1 \
 --opacity 0.3 \
---overwrite \
---target_classes 2 \
---source_class 1 \
---seed 2
+--target_classes 0 \
+--source_class 2 \
+--seed 2 \
+--overwrite
